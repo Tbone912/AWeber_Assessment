2c3537b3208180657f800c515997a99db8694ef9
Tbone912 <56204801+Tbone912@users.noreply.github.com>
2022-12-15 13:12:51 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+"""
+The application must satisfy these requirements:
+
+    Written in Python 3.8 or later.
+    Endpoints to create, read, list, update, and delete objects called "Widgets"
+    Widget objects include the following properties (at least):
+        Name (utf8 string, limited to 64 chars)
+        Number of parts (integer)
+        Created date (date, automatically set)
+        Updated date (date, automatically set)
+    Widgets are persisted to and retrieved from a SQLite file database.
+    Include a README that describes how to setup and run the application.
+
+Ideas to make the project even better:
+
+    Include unit or functional test coverage
+    Include an OpenAPI spec  -  make a swagger
+    PEP8 compliance  - No spaghetti!
+    Pass standard lint tests (ie: flake8 or similar)  -  like SonarLint (Don't make it buggy)
+    Pass bandit security analysis  -  Able to pass CheckMarx
+    Use Python type annotations """
+
+
+#Database should be in local memory
+#Maybe use flask for a clean front end?
+
+
+
+from frontEnd import *
+
+
+
+
+
+if __name__ == '__main__':
+ #  startDatabase()
+ #   insert()
+ #   update()
+ #   read()
+ #readAll()
+ #   delete()
+  app.run(debug = True)
